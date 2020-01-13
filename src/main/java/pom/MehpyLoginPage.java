422fb7bca2166f3c4709105ab3ae66f484ad03c1
vinayak <vinayak@tech.internal>
2020-01-13 16:12:23 +0530

    my export tests,Comments by Admin a Test Sage User

---
+++ src/main/java/pom/MehpyLoginPage.java
@@ -0,0 +1,45 @@
+
+package pom;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindAll;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
+
+
+
+import reuseablePackage.feature.AllActions;
+
+
+public class MehpyLoginPage {
+
+// public WebDriver driver=null;
+// public MehpyLoginPage (WebDriver driver)
+// 	{
+// 		this.driver = driver;
+// 		PageFactory.initElements(AllActions.driver, this);        // Reusable is a Reusable class where we initalize Web Driver ( public static WebDriver driver =null)
+//     }
+    
+ThreadLocal<WebDriver> driver;
+	 	public MehpyLoginPage (ThreadLocal<WebDriver> driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver.get(), this);    
+	}	
+
+
+@FindBy(id="userName")public WebElement username;
+
+@FindBy(id="password")public WebElement password;
+
+@FindBy(id="loginBtn")public WebElement login;
+//pomStart
+	
+
+
+}
